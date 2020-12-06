cb9f304fd4347bc81f494057220a5e0fe162a6f7
KrzysztofKaluza <krzysztof.kaluza.96@gmail.com>
2020-12-06 20:37:16 +0100

    const declaration

--- lab7/SEQUENTIAL_INSTRUCTION_EXECUTE_UNIT/SEQUENTIAL_INSTRUCTION_EXECUTE_UNIT.srcs/sources_1/new/SEQUENTIAL_INSTRUCTION_EXECUTE_UNIT.vhd
+++ lab7/SEQUENTIAL_INSTRUCTION_EXECUTE_UNIT/SEQUENTIAL_INSTRUCTION_EXECUTE_UNIT.srcs/sources_1/new/SEQUENTIAL_INSTRUCTION_EXECUTE_UNIT.vhd
@@ -10,7 +10,8 @@ entity SEQUENTIAL_INSTRUCTION_EXECUTE_UNIT is
 end SEQUENTIAL_INSTRUCTION_EXECUTE_UNIT;
 
 architecture Behavioral of SEQUENTIAL_INSTRUCTION_EXECUTE_UNIT is
-    type rom_t is array (0 to 31) of std_logic_vector(15 downto 0);
+    --type rom_t is array (0 to 31) of std_logic_vector(15 downto 0);
+    type rom_array is array (0 to 31) of std_logic_vector(15 downto 0);
     type reg_array is array (0 to 7) of std_logic_vector(7 downto 0);
     signal R: reg_array;
     type ram_array is array (0 to 31) of std_logic_vector(7 downto 0);
@@ -27,7 +28,34 @@ architecture Behavioral of SEQUENTIAL_INSTRUCTION_EXECUTE_UNIT is
     constant C_OUTP: std_logic_vector( 7 downto 0) := "00000001";
     constant C_B: std_logic_vector( 7 downto 0) := "00000010";
     constant C_BZ: std_logic_vector( 7 downto 0) := "00000011";
-    constant ROM: rom_t := (C_OUTP & x"FF", C_OUTP & x"55", C_BZ & x"02",C_OUTP & x"00", C_NOP & x"00", C_B & x"00", others => x"0000");
+    constant MC_LDI: std_logic_vector(15 downto 0) := "10000-----------";
+    constant C_LDI: std_logic_vector(4 downto 0) := "10000";
+    constant MC_MOV: std_logic_vector(15 downto 0) := "0000000000------";
+    constant C_MOV: std_logic_vector(9 downto 0) := "0000000000";
+    constant MC_LD: std_logic_vector(15 downto 0) := "0000000001------";
+    constant C_LD: std_logic_vector(9 downto 0) := "0000000001";
+    constant MC_LDS: std_logic_vector(15 downto 0) := "10001-----------";
+    constant C_LDS: std_logic_vector(4 downto 0) := "10001";
+    constant MC_ST: std_logic_vector(15 downto 0) := "0000000010------";
+    constant C_ST: std_logic_vector(9 downto 0) := "0000000010";
+    constant MC_STS: std_logic_vector(15 downto 0) := "10010-----------";
+    constant C_STS: std_logic_vector(4 downto 0) := "10010";
+    --constant ROM: rom_t := (C_OUTP & x"FF", C_OUTP & x"55", C_BZ & x"02",C_OUTP & x"00", C_NOP & x"00", C_B & x"00", others => x"0000");
+    constant ROM: rom_array := (
+        C_LDI & "001" & x"35", -- za³adowanie wartoœci x35 do rejestru R1
+        C_LDI & "100" & x"79", -- za³adowanie wartoœci x79 do rejestru R4
+        C_MOV & "101" & "001", -- przes³anie zawartoœci rejestru R1 do rejestru R5
+        C_LDI & "001" & x"02", -- za³adowanie wartoœci x02 do rejestru R1
+        C_ST & "001" & "100", -- zapisanie zawartoœci rejestru R4 do pamiêci RAM
+        -- pod adres zawarty w R1 (adres x02)
+        C_STS & "100" & x"05", -- zapisanie zawartoœci rejestru R4 do pamiêci RAM
+        -- pod adres x05
+        C_LD & "110" & "001", -- za³adowanie wartoœci z pamiêci RAM spod adresu
+        -- zawartego w rejestrze R1 do rejestru R6
+        C_LDS & "111" & x"05", -- za³adowanie wartoœci z pamiêci RAM spod adresu
+        -- x05 do rejestru R7
+        C_B & x"00", -- skok na poczatek programu
+        others => x"0000");
 begin
 
     process(CLK, RESET)
